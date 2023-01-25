58d5b8d071bc21a47f7c2ababb7560b31fed2019
fowntain <dbjones3@students.wcpss.net>
2023-01-25 17:02:32 -0500

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,26 +1,4 @@
-var theme = window.localStorage.currentTheme;
-
-        $('body').addClass(theme);
-
-        if ($("body").hasClass("night")) {
-            $('.dntoggle').addClass('fa-sun-o');
-            $('.dntoggle').removeClass('fa-moon-o');
-        } else {
-            $('.dntoggle').removeClass('fa-sun-o');
-            $('.dntoggle').addClass('fa-moon-o');
-        }
-
-        $('.dntoggle').click(function() {
-            $('.dntoggle').toggleClass('fa-sun-o');
-            $('.dntoggle').toggleClass('fa-moon-o');
-
-            if ($("body").hasClass("night")) {
-                $('body').toggleClass('night');
-                localStorage.removeItem('currentTheme');
-                localStorage.currentTheme = "day";
-            } else {
-                $('body').toggleClass('night');
-                localStorage.removeItem('currentTheme');
-                localStorage.currentTheme = "night";
-            }
-        });
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode")
+})
