6e5c18a43e17fb6d568c71bc4cc765cd37ad57de
fowntain <dbjones3@students.wcpss.net>
2023-01-25 17:12:58 -0500

    Create rainbowtext.js

---
+++ js/rainbowtext.js
@@ -0,0 +1,25 @@
+(function(){
+  'use strict';
+  
+  var p      = document.getElementsByTagName('h1')[0],
+      chars  = p.textContent.split(''),
+      length = chars.length,
+      shift  = 360 / length,
+      angle  = 0,
+      span, t;
+  
+  p.innerHTML = chars.map(function (char) {
+    return '<span>' + char + '</span>';
+  }).join('');
+  
+  span = p.children;
+  
+  function frame() {
+    for (var i = 0; i < length; i++) {
+      span[i].style.color = 'hsl(' + (angle + Math.floor(i * shift)) + ', 60%, 70%)';
+    }
+    angle++;
+  }
+  
+  t = setInterval(frame, 10);
+}());
