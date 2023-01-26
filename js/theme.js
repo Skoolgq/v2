e4afb90cc419544a9b0f44881eb62d7b5cff56f1
fowntain <dbjones3@students.wcpss.net>
2023-01-26 09:28:02 -0500

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,11 @@
 const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
-  document.body.classList.toggle("dark-mode")
+  const div = document.querySelector('body');
+  if (body.classList.contains('dark-mode')) {
+  document.cookie = "theme=light";
+  document.body.classList.add("dark-mode")
+  } else {
+  document.cookie = "theme=dark";
+  document.body.classList.remove("dark-mode")
+  }
 })
