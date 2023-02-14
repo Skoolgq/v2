4920791b6000a9f1c0d2dd57895a9b68e391f4db
thetatertotgod <95923271+thetatertotgod@users.noreply.github.com>
2023-02-14 01:55:22 +0000

    Add files via upload

---
+++ cdn/frontend/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import App from './App.js'
+
+import './index.css';
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
