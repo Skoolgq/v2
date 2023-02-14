6f6d54ff25591f502baf7b64b9d833f28da221bf
thetatertotgod <95923271+thetatertotgod@users.noreply.github.com>
2023-02-13 21:45:22 -0500

    Delete cdn/doom directory

--- cdn/doom/frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from 'react-dom';
-import App from './App.js'
-
-import './index.css';
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
