a9ccb2ed6d877796cb14ef8c16ea11be2c5c2490
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-17 06:55:58 -0400

    wrap in function

--- js/navbar.js
+++ js/navbar.js
@@ -42,22 +42,23 @@ window.addEventListener('mouseup', (event) => {
   }
 })
 
-if (!localStorage.getItem('cloaker')) localStorage.setItem('cloaker', JSON.stringify({}));
-else {
-  function favicon(src) {
-    let link = document.createElement('link')
-    let oldLink = document.getElementById('dynamic-fav');
+function cloak() {
+  if (localStorage.getItem('cloaker')) {
+    function favicon(src) {
+      let link = document.createElement('link')
+      let oldLink = document.getElementById('dynamic-fav');
     
-    link.id = 'dynamic-fav';
-    link.rel = 'shortcut icon';
-    link.href = src;
-    if (oldLink) document.head.removeChild(oldLink);
-    document.head.appendChild(link);
-  }
+      link.id = 'dynamic-fav';
+      link.rel = 'shortcut icon';
+      link.href = src;
+      if (oldLink) document.head.removeChild(oldLink);
+      document.head.appendChild(link);
+    }
   
-  let cloaker = JSON.parse(localStorage.getItem('cloaker'));
-  if (cloaker.title) document.title = cloaker.title;
-  if (cloaker.icon) favicon(cloaker.icon);
+    let cloaker = JSON.parse(localStorage.getItem('cloaker'));
+    if (cloaker.title) document.title = cloaker.title;
+    if (cloaker.icon) favicon(cloaker.icon);
+  } else localStorage.setItem('cloaker', JSON.stringify({}));
 };
 
 // Open links in mobiles
