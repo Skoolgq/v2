45130eb1b7cbb60a543f74936cdf647d58cdcb88
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-23 22:06:19 -0400

    clean up a bit

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,18 @@
-// check if there's already a theme saved in local storage
-var currentTheme = localStorage.getItem("theme") || "light";
+// check if there's already a theme saved in local storage, default to light
+let currentTheme = localStorage.getItem("theme") || "light";
 
 // apply the saved theme
 document.body.classList.add(currentTheme);
-document.getElementById(currentTheme + "-mode").classList.add("active");
+document.getElementById(`${currentTheme}-mode`).classList.add("active");
 
 // switch theme function
 function switchTheme(theme) {
   document.body.classList.remove("light", "dark");
-  document.body.classList.add(theme);
   document.getElementById("light-mode").classList.remove("active");
   document.getElementById("dark-mode").classList.remove("active");
-  document.getElementById(theme + "-mode").classList.add("active");
+  
+  document.body.classList.add(theme);
+  document.getElementById(`${theme}-mode`).classList.add("active");
+  
   localStorage.setItem("theme", theme);
-}
+};
