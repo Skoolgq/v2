3fc2ff87fc918a7db075d5721a4d0dd5a80e8856
VR <101288516+VillainsRule@users.noreply.github.com>
2023-03-24 23:23:00 -0400

    lol

---
+++ js/all.js
@@ -0,0 +1,3 @@
+console.log('%c%s', 'font-size: 26px;', "welcome to the console");
+console.log('%c%s', 'font-size: 18px;', "you probably don't know why you're here.");
+console.log('%c%s', 'font-size: 14px;', "so just close this panel and carry along. good day!");
