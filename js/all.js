ff468f5290af8a971f8f3e48ee39b7fd046f5859
fowntain <dbjones3@students.wcpss.net>
2023-05-12 08:01:25 -0400

    add 4 for now

--- js/all.js
+++ js/all.js
@@ -2,9 +2,7 @@ console.log('%c%s', 'font-size: 26px;', "welcome to the console");
 console.log('%c%s', 'font-size: 18px;', "you probably don't know why you're here.");
 console.log('%c%s', 'font-size: 14px;', "so just close this panel and carry along. good day!");
 
-if(localStorage.uvBackend == null)
-{
-    localStorage.uvBackend = "obliged-gisele-thetatortotgod.koyeb.app";
-}
+const uvBackendList = ["holyubofficial.net/runtime~react/service/", "metallic.gq/ultraviolet/", "campusdirections.org/runtime~react/service/", "genebelcher.cyclic.app/uv/service/"];
+localStorage.uvBackend = uvBackendList[Math.floor(Math.random() * uvBackendList.length)];
 localStorage.setItem("currentapp", localStorage.currentapp.replace("{proxy}", localStorage.uvBackend))
 localStorage.setItem("currentgame", localStorage.currentapp.replace("{proxy}", localStorage.uvBackend))
