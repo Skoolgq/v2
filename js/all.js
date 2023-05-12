4d7c4906bacfc2152fd6c7cf90754277d4795d85
fowntain <dbjones3@students.wcpss.net>
2023-05-12 09:44:18 -0400

    testing

--- js/all.js
+++ js/all.js
@@ -6,3 +6,17 @@ const uvBackendList = ["holyubofficial.net/runtime~react/service/", "metallic.gq
 localStorage.uvBackend = uvBackendList[Math.floor(Math.random() * uvBackendList.length)];
 localStorage.setItem("currentapp", localStorage.currentapp.replace("{proxy}", localStorage.uvBackend))
 localStorage.setItem("currentgame", localStorage.currentapp.replace("{proxy}", localStorage.uvBackend))
+
+// get the current URL
+const currentUrl = window.location.href;
+// get the ad divs
+const popunderdiv = document.querySelector('#popunder-ad');
+
+// check the URL and replace the div with different HTML code based on the URL
+if (currentUrl.includes('skoolgq.github.io')) {
+  popunderdiv.innerHTML = "<script type='text/javascript' src='//pl19358963.highrevenuegate.com/91/8e/f0/918ef00884db501fa39b827dc69381cd.js'></script>";
+} else if (currentUrl.includes('ela.pages.dev')) {
+  popunderdiv.innerHTML = '<h1>testing code, ignore</h1>';
+} else {
+  popunderdiv.innerHTML = '<h1>Welcome to the Default Website</h1><p>This is the default website.</p>';
+}
